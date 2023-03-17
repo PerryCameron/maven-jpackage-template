95bb551c2b10765873e142cbb2b71e6f70cbfb1f
PerryCameron <pcameron28@yahoo.com>
2023-03-17 12:13:57 -0400

    Formatted model to have a cleaner look

--- src/main/java/com/changenode/Model.java
+++ src/main/java/com/changenode/Model.java
@@ -16,42 +16,21 @@ public class Model {
     private final ObjectProperty<LogData> logData = new SimpleObjectProperty<>();
 
     public ObjectProperty<LogData> logDataProperty() { return logData;}
-    public LogData getLogData() {
-        return logData.get();
-    }
-    public void setLogData(LogData logData) {
-        this.logData.set(logData);
-    }
-    public boolean isFileChooserOpen() {
-        return fileChooserOpen.get();
-    }
-    public BooleanProperty fileChooserOpenProperty() {
-        return fileChooserOpen;
-    }
-    public void setFileChooserOpen(boolean fileChooserOpen) {
-        this.fileChooserOpen.set(fileChooserOpen);
-    }
-    public StringProperty mainTextProperty() {
-        return mainText;
-    }
-    public void setMainText(String mainText) {
-        this.mainText.set(mainText);
-    }
-    public StringProperty statusLabelProperty() {
-        return statusLabel;
-    }
-    public void setStatusLabel(String statusLabel) {
-        this.statusLabel.set(statusLabel);
-    }
+    public LogData getLogData() { return logData.get(); }
+    public void setLogData(LogData logData) { this.logData.set(logData); }
+    public boolean isFileChooserOpen() { return fileChooserOpen.get(); }
+    public BooleanProperty fileChooserOpenProperty() { return fileChooserOpen; }
+    public void setFileChooserOpen(boolean fileChooserOpen) { this.fileChooserOpen.set(fileChooserOpen); }
+    public StringProperty mainTextProperty() { return mainText; }
+    public void setMainText(String mainText) { this.mainText.set(mainText); }
+    public StringProperty statusLabelProperty() { return statusLabel; }
+    public void setStatusLabel(String statusLabel) { this.statusLabel.set(statusLabel); }
     public BooleanProperty isDarkProperty() { return isDark; }
     public void setIsDark(boolean isDark) { this.isDark.set(isDark); }
-
     public IntegerProperty currentIconProgressProperty() { return currentIconProgress; }
     public void setCurrentIconProgress(int currentIconProgress) { this.currentIconProgress.set(currentIconProgress); }
-
     public ObjectProperty<BufferedImage> defaultIconProperty() { return defaultIcon; }
     public void setDefaultIcon(BufferedImage defaultIcon) { this.defaultIcon.set(defaultIcon); }
-
     public ObjectProperty<BufferedImage> redCircleIconProperty() { return redCircleIcon; }
     public void setRedCircleIcon(BufferedImage redCircleIcon) { this.redCircleIcon.set(redCircleIcon); }
 }
